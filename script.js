4a53b6b731e29527e9f184887bbf41830598ff68
Kauan-D <kauan.79977@aluno.pr.senac.br>
2026-02-10 18:07:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let nomeUser = prompt("diga seu nome: ")
+let idadeUser = prompt("diga sua idade: ")
+let cidadeUser = prompt("diga sua cidade: ")
+
+
+//Comando de output para o desenvolvedor testar saidas.
+console.log("Olá Mundo");
+console.log("o senhor/senhora, confirma que sua cidade é: " + cidadeUser + ", seu nome é " + nomeUser + ", e sua idade é " + idadeUser + "?")
+console.log(nomeUser + ", bem-vindo ao site.")
+
+//Comando de output para o usuario final.
+alert("Olá Mundo")
+alert("o senhor/senhora, coonfirma que sua cidade é: " + cidadeUser + ", seu nome é " + nomeUser + ", e sua idade é " + idadeUser + "?")
+alert(nomeUser + ", bem-vindo ao site.")
