d9d023f9a76e22b17b0610bb08699dfacf604491
Kauan-D <kauan.79977@aluno.pr.senac.br>
2026-02-13 18:10:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let idadeUser = prompt("digite sua idade: ");
-if (idadeUser >= 18) {
-    alert("Você pode apostar, o quanto quiser");
+let estadoUser = prompt("digite seu estado: ").toUpperCase();
+
+if (idadeUser >= 18 && estadoUser === "PR") {
+    document.getElementById("eita").innerText = "você esta apto para iniciar o teste."
+    document.getElementById("lascouPerdiTudo").innerText = "Parabéns!";
+    document.getElementById("aposta").innerText = "Vá até o detran mais proximo, e tire sua carteira"
 } else {
-    alert("Tu é de menor, vaza daqui");
+    document.getElementById("lascouPerdiTudo").innerText = "infelizmente você não pode fazer."
+    document.getElementById("eita").innerText = "tente no futuro!"
+    document.getElementById("aposta").innerText = "Um abraço até logo!"
 }
